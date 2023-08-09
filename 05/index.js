4ab957d2d8cb2d3e5e02eb26dce8d6a0e19c49eb
Luan Barradas <barradas.luk@gmail.com>
2023-08-09 19:35:35 -0300

    exercicio 05 resolvido

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,13 @@
 const numeroCartao = '1111222233334444';
+
+const numerosInicio = numeroCartao.slice(0, 4);
+const numerosFim = numeroCartao.slice(-4);
+
+let numerosMeio = '';
+for (let i = 0; i < numeroCartao.length -8; i++) {
+    numerosMeio += '*';
+}
+
+const cartaoModificado = numerosInicio + numerosMeio + numerosFim;
+
+console.log(cartaoModificado);
