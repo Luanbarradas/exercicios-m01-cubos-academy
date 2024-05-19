48e40737ae9ce4ea18ce9365d18fcd2874bdd347
Luanbarradas <barradas.luk@gmail.com>
2024-05-19 20:46:32 -0300

    feat: solving missing exercises

--- 06/index.js
+++ 06/index.js
@@ -1,1 +1,30 @@
 const celular = 7199918888;
+
+const formataCelular = (numero) => {
+  let celular;
+
+  if (numero.length === 8) {
+    celular = `9 ${numero.slice(0, 1)} ${numero.slice(1, 5)}-${numero.slice(
+      5
+    )}`;
+    console.log(celular);
+  } else if (numero.length === 9) {
+    celular = `${numero.slice(0, 1)} ${numero.slice(1, 5)}-${numero.slice(5)}`;
+    console.log(celular);
+  } else if (numero.length === 10) {
+    celular = `${numero.slice(0, 2)} 9 ${numero.slice(2, 6)}-${numero.slice(
+      6
+    )}`;
+    console.log(celular);
+  } else if (numero.length === 11) {
+    celular = `(${numero.slice(0, 2)}) ${numero.slice(2, 3)} ${numero.slice(
+      3,
+      7
+    )}-${numero.slice(7)}`;
+    console.log(celular);
+  } else {
+    console.log("Número inválido");
+  }
+};
+
+formataCelular(celular.toString());
--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,28 @@
-const email = "aluno@cubos.academy";
+const validarEmail = (email) => {
+  let emailValido = true;
+
+  if (
+    email.indexOf("@") < 0 ||
+    email.indexOf(".") < 0 ||
+    email.indexOf(".") === 0 ||
+    email.lastIndexOf(".") === email.length - 1
+  ) {
+    emailValido = false;
+  }
+
+  console.log(emailValido ? "email válido" : "email inválido");
+};
+
+validarEmail("jose@cubos.academy"); //valido
+validarEmail("jose@cubos.academy.br"); //valido
+validarEmail("jose.messias@cubos.academy"); //valido
+validarEmail("jose.messias@cubos.io"); //valido
+validarEmail("jose@cubos"); //invalido
+validarEmail("jose.messias@cubos"); //valido
+validarEmail("jose.messias@."); //invalido
+validarEmail("jose.@cubos"); //valido
+validarEmail(".@"); //invalido
+validarEmail("@."); //invalido
+validarEmail("jose.messias@cubos."); //invalido
+validarEmail(".messias@cubos."); //invalido
+validarEmail(".messias@cubos"); //invalido
